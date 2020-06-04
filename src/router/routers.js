c88ba562bc6497ff4d89ce7ca02dd9297b62ecb6
Tien Lam Huu <petien25@gmail.com>
2020-06-04 23:27:09 +0700

    Upload UI for customer, ui for profile, account and sidebar, navbar for customer dashboard

--- src/router/routers.js
+++ src/router/routers.js
@@ -10,6 +10,11 @@ import DashboardAdminCmp from "../components/admin/DashboardAdminCmp"
 import ListEmployeeCmp from '../components/admin/customer/ListEmployeeCmp'
 import ListPartnerCmp from '../components/admin/customer/ListPartnerCmp'
 import ListTransactionCmp from '../components/admin/customer/ListTransactionCmp'
+
+//Customer
+import DashboardCustomerCmp from '../components/customer/DashboardCustomerCmp'
+import ListAccountCmp from '../components/customer/account/ListAccountCmp'
+import CustomerProfile from '../components/customer/account/CustomerProfile'
 Vue.use(VueRouter)
 
 
@@ -32,6 +37,20 @@ let routes = [
             {
                 path: "/admin/transation",
                 component: ListTransactionCmp
+            },
+        ]
+    },
+    {
+        path: "/customer",
+        component: DashboardCustomerCmp,
+        children: [
+            {
+                path: "/customer/account",
+                component: ListAccountCmp
+            },
+            {
+                path: "/customer/profile",
+                component: CustomerProfile
             },
         ]
     },
